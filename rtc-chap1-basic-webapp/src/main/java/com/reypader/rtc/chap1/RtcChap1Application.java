ae6731efc8b7a28d9f4dc1caade0ef0454ae0848
rmpader <reynaldmpader@gmail.com>
2024-09-22 20:37:55 +0800

    Reconfigure ObjectMapper to properly handle JavaTime classes

--- rtc-chap1-basic-webapp/src/main/java/com/reypader/rtc/chap1/RtcChap1Application.java
+++ rtc-chap1-basic-webapp/src/main/java/com/reypader/rtc/chap1/RtcChap1Application.java
@@ -2,7 +2,13 @@ package com.reypader.rtc.chap1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @SpringBootApplication
 @Configuration
@@ -10,6 +16,15 @@ public class RtcChap1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RtcChap1Application.class, args);
+	}
+
+	@Primary
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		return objectMapper;
 	}
 
 }
