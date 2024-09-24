a49646adffdc6f945e5333fac16c5e8b3f9e4071
rmpader <reynaldmpader@gmail.com>
2024-09-24 22:15:59 +0800

    Add basic spring context test

--- rtc-chap3-unit-tests/src/test/java/com/reypader/rtc/chap3/RtcChap3PersistenceApplicationTests.java
+++ rtc-chap3-unit-tests/src/test/java/com/reypader/rtc/chap3/RtcChap3PersistenceApplicationTests.java
@@ -1,15 +1,24 @@
 package com.reypader.rtc.chap3;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import com.reypader.rtc.chap3.controllers.EventController;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class RtcChap3PersistenceApplicationTests {
 
+	@Autowired
+	private EventController eventController;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(eventController);
 	}
 
 }
